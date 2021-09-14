945f60f0158627ffaaef03d119efd7e49721ef8a
bxvan07 <90613143+bxvan07@users.noreply.github.com>
2021-09-14 21:09:14 +0530

    Update banksystem.sql

--- banksystem.sql
+++ banksystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 13, 2021 at 12:25 PM
+-- Generation Time: Sep 14, 2021 at 05:38 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -32,7 +32,7 @@ CREATE TABLE `transaction` (
   `sender` varchar(20) NOT NULL,
   `receiver` varchar(20) NOT NULL,
   `balance` int(8) NOT NULL,
-  `datetime` int(8) NOT NULL DEFAULT current_timestamp()
+  `datetime` datetime(6) NOT NULL DEFAULT current_timestamp(6)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -40,14 +40,11 @@ CREATE TABLE `transaction` (
 --
 
 INSERT INTO `transaction` (`sno`, `sender`, `receiver`, `balance`, `datetime`) VALUES
-(0, 'Nemo ', 'ron', 200, 2147483647),
-(0, 'Nemo ', 'Gaurav', 22, 2147483647),
-(0, 'Gaurav', 'Nemo ', 1200, 2147483647),
-(0, 'Saneet', 'Nemo ', 1222, 2147483647),
-(0, 'Saneet', 'Suvam', 666, 2147483647),
-(0, 'Niky', 'goku', 122, 2147483647),
-(0, 'zoro', 'lia', 22222, 2147483647),
-(0, 'zoro', 'goku', 2334, 2147483647);
+(0, 'zoro', 'lia', 2, '2021-09-14 21:05:05.222286'),
+(0, 'Niky', 'leo', 2000, '2021-09-14 21:06:07.871156'),
+(0, 'leo', 'San', 5000, '2021-09-14 21:06:30.434552'),
+(0, 'San', 'lia', 555, '2021-09-14 21:06:44.860571'),
+(0, 'goku', 'prati', 5000, '2021-09-14 21:07:07.842467');
 
 -- --------------------------------------------------------
 
@@ -67,16 +64,16 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
-(1087, 'goku', 'goku@gmail.com', 49056),
-(2654, 'zoro', 'zoro@gmail.com', 6064),
+(1087, 'goku', 'goku@gmail.com', 44056),
+(2654, 'zoro', 'zoro@gmail.com', 5939),
 (3624, 'ron', 'nemo@gmail.com', 40000),
-(4542, 'lia', 'lia@gmail.com', 71022),
-(5345, 'San', 'sa@gmail.com', 35000),
-(5465, 'as', 'ba@gmail.com', 49500),
-(6554, 'Niky', 'loveday@gmail.com', 39878),
-(7875, 'prati', 'pa@gmail.com', 49480),
-(8877, 'ditto', 'dittool@gmail.com', 40000),
-(9245, 'leo', 'nalla@gmail.com', 30000);
+(4542, 'lia', 'lia@gmail.com', 71579),
+(5345, 'San', 'sa@gmail.com', 39446),
+(5465, 'as', 'ba@gmail.com', 49499),
+(6554, 'Niky', 'loveday@gmail.com', 37545),
+(7875, 'prati', 'pa@gmail.com', 54480),
+(8877, 'ditto', 'dittool@gmail.com', 40456),
+(9245, 'leo', 'nalla@gmail.com', 27000);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
